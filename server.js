ea9f668a07f60a27719cefa4e0f0efe7cadb3323
PremoliAlessandro <aleprem212@gmail.com>
2022-05-18 12:48:08 +0200

    add orders finto db

--- server.js
+++ server.js
@@ -34,6 +34,55 @@ app.get("/orders", (req, res) => {
         },
       ],
     },
+    {
+      id: 2,
+      numero: "845-003133-4590",
+      dataOrdine: "2022-03-29",
+      totale: 299.88,
+      cliente: {
+        id: 298,
+        nominativo: "genitore 2",
+      },
+      listProdotti: [
+        {
+          id: 11,
+          descr: "descr1111",
+          prezzo: 99.98,
+        },
+        {
+          id: 66,
+          descr: "descr66",
+          prezzo: 159.9,
+        },
+      ],
+    },
+    {
+      id: 3,
+      numero: "845-001-4590",
+      dataOrdine: "2022-03-27",
+      totale: 129.88,
+      cliente: {
+        id: 270,
+        nominativo: "ciaoooo ciao",
+      },
+      listProdotti: [
+        {
+          id: 9,
+          descr: "descr9",
+          prezzo: 99.98,
+        },
+        {
+          id: 66,
+          descr: "descr66",
+          prezzo: 159.9,
+        },
+        {
+          id: 67,
+          descr: "descruueu",
+          prezzo: 159.9,
+        },
+      ],
+    },
   ];
   res.send(fakeOrders);
 });
